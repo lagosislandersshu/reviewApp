16bb3a30fb4d14418b5dba9bf8662386b27776fb
babaniyilawal <babaniyilawal@yahoo.com>
2023-04-08 22:20:03 +0000

    Product, Addproduct, EditProduct

--- reviewApp/alpha/urls.py
+++ reviewApp/alpha/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),   
-    path('contact/', views.contact, name='contact'),
+    path('', views.home, name='home'),    
+    path('alpha/addProduct/', views.addProduct, name='addProduct'),
+    path('alpha/editProduct/<int:pk>/', views.editProduct, name='editProduct'),
+    path('alpha/deleteProduct/<int:pk>/', views.deleteProduct, name='deleteProduct'),
+    path('alpha/product/', views.product, name='product'), 
+    path('alpha/contact/', views.contact, name='contact'),
+      
 ]

a2dd443f9513924e7d60e1f7b0933b1aaaa0a081
lagosislandersshu <lagosislandersshu@gmail.com>
2023-04-24 15:39:25 +0000

    deployment config

--- reviewproject/settings.py
+++ reviewproject/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ymemjubln0!i@wf7ggt22e6__5qw8y_+zt_or5@crsu602-6gh'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ymemjubln0!i@wf7ggt22e6__5qw8y_+zt_or5@crsu602-6gh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'WEBSITE_HOSTNAME' not in os.environ
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+    CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 
 
 # Application definition
